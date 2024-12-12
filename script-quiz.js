452488c36b8d2dd1315bbaaed12b08379eef1e19
Fenris Janghede <fenrisjanghede@outlook.com>
2024-12-12 15:27:09 +0100

    print coloured results based on if/else score

--- script-quiz.js
+++ script-quiz.js
@@ -1,4 +1,3 @@
-
 const proceedBtn = document.querySelector("#popup-confirm #proceed-btn");
 proceedBtn.addEventListener("click", (e) => {
 	e.preventDefault();
@@ -13,17 +12,6 @@ proceedBtn.addEventListener("click", (e) => {
 			div.style.display = "none";
 		}
 	});
-
-	// #region Trial n Error
-		// let trials = document.querySelector("#quiz-form").children;
-		// quizDivs = Array.from(trials);
-		// console.log(trials, quizDivs);
-
-	// let parentDiv = e.target.parentElement.parentElement;
-	// console.log(parentDiv)
-	// let nextDiv = parentDiv.nextElementSibling;
-	// console.log(nextDiv);
-	//#endregion
 });
 
 const questions = [
@@ -52,69 +40,57 @@ const correctAnswers = [
 ];
 let correctArray = [];
 let incorrectArray = [];
-
 let fieldsetNodes = document.querySelectorAll("fieldset .question");
 let fieldsetArray = Array.from(fieldsetNodes);
+let submitBtn = document.querySelector("#submitBtn");
+// submitBtn.style.display = "none";  // TEMPLORARILY COMMENTED -
 
-		let submitBtn = document.querySelector("#submitBtn");
-		submitBtn.style.display = "none";
-
-// loop through questions, forEach arrow-function //
+// Loop through questions, forEach arrow-function //
 questions.forEach((question, index) => {
 	fieldsetArray[index].innerText = question;
 	index++;
-	console.log(index, question);
+	console.log(questions[2]); // TEMP //
 });
 
 let lockInBtn = document.querySelectorAll("fieldset .lockIn-btn");
 let lockinBtns = Array.from(lockInBtn);
 
-// #region // loop through lockinBtns, forEach arrow-function //
+// #region - Loop through lockinBtns, forEach arrow-function //
 lockinBtns.forEach((button, index) => {
 	button.textContent = "Lock in answer";
 
 	//eventListener "click" arrow function
 	button.addEventListener("click", (e) => {
 		e.preventDefault();
-
-		// console.log(e.target.lastChild);
 		let questionDiv = button.closest(".question-div");
-    // console.log(questionDiv)
 
 		let selectedRadio = questionDiv.querySelector(
 			"input[type='radio']:checked"
 		);
+
 		if (!selectedRadio) {
 			alert("Make a selection before locking in your answer.");
 			return;
-		} 
-		else if (selectedRadio) {
+		} else if (selectedRadio) {
 			let userinput = selectedRadio.value;
 			let correctanswer = correctAnswers[index];
-			console.log(index + 1, "User answer: " + userinput);
-			console.log(index + 1, "Correct answer: " + correctanswer);
 
 			if (userinput === correctanswer) {
 				correctArray.push(index + 1);
-				console.log(correctArray);
-			} 
-			else {
+			} else {
 				incorrectArray.push(index + 1);
-        console.log(incorrectArray);
 				console.log("Question " + (index + 1) + " incorrect.");
 			}
 
 			let prevQuestion = e.target.parentElement.parentElement;
+			prevQuestion.style.opacity = "0.5";
 			let activeQuestion = prevQuestion.nextElementSibling;
-			let nextQuestion = activeQuestion.nextElementSibling;
-			console.log(prevQuestion, activeQuestion, nextQuestion);
-
 			activeQuestion.style.display = "block";
 			activeQuestion.style.opacity = "1";
-			prevQuestion.style.opacity = "0.5";
+			let nextQuestion = activeQuestion.nextElementSibling;
 		}
 
-		// loop through radio-btns' value, forEach arrow-function //
+		// Loop through radio-btns' value, forEach arrow-function //
 		let radiobuttons = questionDiv
 			.querySelectorAll("input[type='radio']")
 			.forEach((radio) => {
@@ -126,7 +102,8 @@ lockinBtns.forEach((button, index) => {
 		button.style.color = "grey";
 		button.style.fontStyle = "italic";
 
-		if (index === questions.length) { // index === 10
+		if (index === questions.length) {
+			// index === 10
 			submitBtn.style.display = "block";
 		}
 	});
@@ -134,47 +111,60 @@ lockinBtns.forEach((button, index) => {
 //#endregion
 
 let quizResults = document.querySelector("#quiz-results");
-quizResults.style.backgroundColor = "red";
+// quizResults.style.backgroundColor = "red";
 quizResults.style.display = "none";
 
-// quizResults.style.display = "block"; // TEMP
-
-//#region 
-		// let resultsP = document.querySelectorAll("#quiz-results p");
-		// // let resultsCorrect = document.querySelector("#quiz-results p");
-		// // let resultsIncorrect = document.querySelector("#quiz-results p");
-		// resultsP[0].append(correctArray.length + " correct answers");
-		// resultsP[1].append(incorrectArray.length + " incorrect answers");
-
-		// let resultsTitle = document.querySelector("#quiz-results h2");
-		// resultsTitle.append("your results are...");
-		// resultsCorrect.append(correctArray.length + " correct answers");
-		// resultsIncorrect.append(incorrectArray.length + " incorrect answers");
-
-		// console.log(quizResults);
-//#endregion
-
-
 //MAKE INTO POPUP-WINDOW:
-	submitBtn.addEventListener("click", (e) => {
-		e.preventDefault();
-		let resultsP = document.querySelectorAll("#quiz-results p");
-		// let resultsCorrect = document.querySelector("#quiz-results p");
-		// let resultsIncorrect = document.querySelector("#quiz-results p");
-		resultsP[0].append(correctArray.length + " correct answers");
-		resultsP[1].append(incorrectArray.length + " incorrect answers");
-		let resultsTitle = document.querySelector("#quiz-results h2");
-		resultsTitle.append("your results are...");
-		// let resultsP = document.querySelector("#quiz-results p")
-		// quizResults.style.display = "block";
-		// resultsP.append("your results are");
-		// console.log(resultsP)
+submitBtn.addEventListener("click", (e) => {
+	e.preventDefault();
 
-		// let resultsDiv = document.createElement("div");
-		// resultsDiv.classList.add("resultsDiv");
-		// resultsDiv.style.backgroundColor = "red";
-		// resultsDiv.innerHTML = "<p>your results!</p>"
-		// console.log(resultsDiv);
-		// resultsDiv.style.display = "block";
+	quizResults.style.display = "block"; // TEMP
 
-	})
+	let resultsP = document.querySelectorAll("#quiz-results p");
+	resultsP[0].append(correctArray.length + " correct answers");
+	resultsP[1].append(incorrectArray.length + " incorrect answers");
+
+	let resultsTitle = document.querySelector("#quiz-results h2");
+	resultsTitle.append("Your results are...");
+
+	let score = correctArray.length;
+	let totalQuestions = correctAnswers.length;
+	let percentage = Math.round((score / totalQuestions) * 100 * 10) / 10;
+
+	let percentageRoundedUp = Math.round(percentage) + "%"; // no decimals
+	resultsP[2].append(
+		"Your score: " + correctArray.length + "/" + totalQuestions
+	);
+	resultsP[2].append(" (" + percentageRoundedUp + "%)");
+
+	if (percentage < 50) {
+		resultsP[2].style.color = "red";
+		resultsP[2].style.fontStyle = "italic";
+		resultsP[2].style.fontStyle = "bold";
+		resultsP[2].innerHTML += "</br> Failed";
+	} else if (percentage <= 75) {
+		resultsP[2].style.color = "orange";
+		resultsP[2].style.fontStyle = "italic";
+		resultsP[2].innerHTML += "</br> Passable </br>";
+	} else {
+		resultsP[2].style.color = "green";
+		resultsP[2].style.fontStyle = "italic";
+		resultsP[2].innerHTML += "</br> nicely done!";
+	}
+
+	console.log(
+		score,
+		totalQuestions,
+		resultsP[0],
+		resultsP[1],
+		resultsP[2],
+		percentage,
+		percentageRoundedUp
+	);
+});
+// let score = 0;
+// let totalQuestions = 15;
+// let percentage = Math.round((score / totalQuestions) * 100 * 10) / 10;
+// let percentage = ((score / totalQuestions) * 100).toFixed(1);
+// let percentageRoundedUp = (Math.round(percentage * 10) / 10) + "%";  // 1 decimal
+// let percentageRoundedUp = Math.round(percentage) + "%"; // no decimals
